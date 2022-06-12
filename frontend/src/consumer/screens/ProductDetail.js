602b562431c8df2bbe67453b2b2107ff79c3813e
BhoneAK <bhoneak@outlook.com>
2022-06-12 14:14:46 +0630

    feat: Consumer UI Implementation - add ProductDetail component

---
+++ frontend/src/consumer/screens/ProductDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../components/common/Header/Header";
+import Info from "../components/product-detail/Info";
+import Footer from "../components/common/Footer/Footer";
+const ProductDetail = () => {
+
+    return (
+        <>
+            <Header />
+            <Info />
+            <Footer />
+        </>
+    )
+}
+export default ProductDetail;
