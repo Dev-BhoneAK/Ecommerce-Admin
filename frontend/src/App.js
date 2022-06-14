c70e92e73cab7288ed7a922cf8ebb8b583e71b28
BhoneAK <bhoneak@outlook.com>
2022-06-15 00:12:02 +0630

    feat: Consumer UI Implementation - add ScrollToTop component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
+import {Route,Routes} from 'react-router-dom';
 import Home from "./consumer/screens/Home";
 import ProductDetail from "./consumer/screens/ProductDetail";
 
 function App() {
-  return  <>
-            <ProductDetail />
-          </>;
+  return  <Routes>
+            <Route path="/" exact index element={<Home />} />
+            <Route path="product/:id" element={<ProductDetail />} />
+          </Routes>;
 }
 
 export default App;
