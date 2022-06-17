812b5056a7462027c8bd48b2d43d8cd7cb1e5069
BhoneAK <bhoneak@outlook.com>
2022-06-17 15:29:19 +0630

    feat: Admin UI Implementation - add Home component

---
+++ admin/src/screens/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Dashboard from "../components/common/Dashboard";
+
+const Home = () => {
+
+    return (
+        <Dashboard>
+            <div className="container-fluid">
+                <div className="page-header">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="page-header-left">
+                                <h3>
+                                    Home
+                                    <small>Multikart Admin panel</small>
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <ol className="breadcrumb pull-right">
+                                <li className="breadcrumb-item">
+                                    <a href="index.html">
+                                        <i className="fa fa-home" />
+                                    </a>
+                                </li>
+                                <li className="breadcrumb-item active">Home</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Dashboard>
+    )
+}
+export default Home;
---
+++ admin/src/utility/mockedMenus.js
@@ -0,0 +1,81 @@
+export const menuItems = [
+
+    {
+        name: "Home",
+        icon: "fa-home"
+    },
+    {
+        name: "Category",
+        icon: "fa-list-ul",
+        submenu: [
+            {
+                name: "Category List",
+            },
+            {
+                name: "Add Category",
+            }
+        ]
+    },
+    {
+        name: "Products",
+        icon: "fa-archive",
+        submenu: [
+            {
+                name: "Product List"
+            },
+            {
+                name: "Add Product",
+            },
+            {
+                name: "Product Review",
+            }
+        ]
+    },
+    {
+        name: "Orders",
+        icon: "fa-shopping-cart",
+        submenu: [
+            {
+                name: "Order List"
+            },
+            {
+                name: "Order Tracking",
+            },
+            {
+                name: "Order Details"
+            },
+        ]
+    },
+    {
+        name: "Sales",
+        icon: "fa-money",
+        submenu: [
+            {
+                name: "Orders"
+            },
+            {
+                name: "Transactions",
+            },
+        ]
+    },
+    {
+        name: "Users",
+        icon: "fa-user",
+        submenu: [
+            {
+                name: "User List"
+            },
+            {
+                name: "Create User",
+            }
+        ]
+    },
+    {
+        name: "Reports",
+        icon: "fa-line-chart"
+    },
+    {
+        name: "Invoices",
+        icon: "fa-file-text"
+    },
+];
