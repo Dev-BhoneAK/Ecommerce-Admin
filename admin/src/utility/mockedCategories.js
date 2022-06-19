ccd466149c6e3e025e4e0cd1c423e8560ee056ed
BhoneAK <bhoneak@outlook.com>
2022-06-19 23:08:29 +0630

    feat: Admin UI Implementation - Add Mocked Categories Utility for CategoryList Component

---
+++ admin/src/utility/mockedCategories.js
@@ -0,0 +1,73 @@
+export const mockedCategories = [
+
+    {
+        id: 1,
+        name: "Home",
+        price: "300"
+    },
+    {
+        id: 2,
+        name: "Away",
+        price: "300"
+    },
+    {
+        id: 3,
+        name: "Office",
+        price: "300"
+    },
+    {
+        id: 4,
+        name: "Beat",
+        price: "300"
+    },
+    {
+        id: 5,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 6,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 7,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 8,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 9,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 10,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 11,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 12,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 13,
+        name: "Nowhere",
+        price: "300"
+    },
+    {
+        id: 14,
+        name: "Nowhere",
+        price: "300"
+    },
+];
