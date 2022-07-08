5952976d8a05cf4a1fd2e0b61a1027f2297682ce
BhoneAK <bhoneak@outlook.com>
2022-07-08 23:47:08 +0630

    feat: Admin UI Implementation - add formatFormData as helper function

--- admin/src/features/category/CreateEditForm.js
+++ admin/src/features/category/CreateEditForm.js
@@ -1,149 +1,185 @@
-import React,{useContext, useEffect, useState} from 'react';
-import {useDispatch} from "react-redux";
-import {ErrorMessage, Field, Form, Formik} from "formik";
+import React, { useContext, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
-import {CreateEditContext} from './CreateEditContext';
-import {createCategory, updateCategory} from './categorySlice';
+import { CreateEditContext } from "./CreateEditContext";
+import { createCategory, updateCategory } from "./categorySlice";
 
 const CreateEditForm = () => {
+  const dispatch = useDispatch();
+  const createEditContext = useContext(CreateEditContext);
+  const { categoryItems, toggleModal, id } = createEditContext;
+  const [statusMessage, setStatusMessage] = useState({
+    status: "",
+    message: "",
+  });
+  const [initialValues, setInitialValues] = useState({
+    name: "",
+    parent: "",
+  });
 
-    const dispatch = useDispatch();
-    const createEditContext = useContext(CreateEditContext);
-    const {categoryItems, toggleModal, id} = createEditContext;
-    const [statusMessage, setStatusMessage] = useState({
-        status: '',
-        message: ''
-    });
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        parent: ''
-    });
-
-    useEffect(() => {
-
-        if (id !== 0) {
-            const category = categoryItems.filter(category => category._id === id)[0];
-            // Object.keys(initialValues).map((key) => {
-            //     console.log(key);
-            //     setInitialValues(prevState => ({
-            //         ...prevState,
-            //         [key]: category[key]
-            //     }))
-            // })
-            // const objKeys = Object.keys(initialValues).forEach((key) => ({
-            //     [key]: category[key]
-            // }));
-            const objKeys = Object.keys(initialValues).reduce((obj, char, index) => {
-                obj[char] = category[char];
-                return obj;
-            }, {});
-            console.log('objKeys ', objKeys);
-            // setInitialValues(prevState => (Object.keys(initialValues).map((key) => ({ ...prevState, [key]: category[key] }))));
-            // setInitialValues(Object.keys(initialValues).map((key, prevState) =>
-            //     ({ ...prevState, [key]: category[key] })));
-                setInitialValues(prevState => ({
-                    ...prevState,
-                    ...objKeys
-                }))
-        }
-    }, [id]);
-    const validationSchema = Yup.object().shape({
-        name: Yup.string()
-            .required('Name is required')
-    });
-
-    const onSubmit = async (categoryFields, {resetForm, setSubmitting}) => {
-        console.log('onSubmit ', categoryFields);
-        categoryFields.path = (categoryFields.parent === '' ? '/' : categoryFields.parent)+ '/' + categoryFields.name;
-        try {
-            let message = '';
-            if (id === 0) {
-                await dispatch(createCategory(categoryFields)).unwrap();
-                message = 'Category Created Successfully!';
-            }else {
-                await dispatch(updateCategory({id, categoryFields})).unwrap();
-                message = 'Category Updated Successfully!';
-            }
-            setStatusMessage(prevState => ({
-                ...prevState,
-                status : 'success',
-                message: message
-            }));
-            resetForm();
-        } catch (err) {
-            console.error('Failed to save the post: ', err);
-            setStatusMessage(prevState => ({
-                ...prevState,
-                status : 'error',
-                message: err.name +': ' + err.message
-            }));
-            setSubmitting(false);
-        } finally {
-            setTimeout(() => {
-                setStatusMessage({
-                    name: '',
-                    parent: ''
-                })
-            }, 2000);
-        }
+  useEffect(() => {
+    if (id !== 0) {
+      const category = categoryItems.filter(
+        (category) => category._id === id
+      )[0];
+      // Object.keys(initialValues).map((key) => {
+      //     console.log(key);
+      //     setInitialValues(prevState => ({
+      //         ...prevState,
+      //         [key]: category[key]
+      //     }))
+      // })
+      // const objKeys = Object.keys(initialValues).forEach((key) => ({
+      //     [key]: category[key]
+      // }));
+      const objKeys = Object.keys(initialValues).reduce((obj, char, index) => {
+        obj[char] = category[char];
+        return obj;
+      }, {});
+      console.log("objKeys ", objKeys);
+      // setInitialValues(prevState => (Object.keys(initialValues).map((key) => ({ ...prevState, [key]: category[key] }))));
+      // setInitialValues(Object.keys(initialValues).map((key, prevState) =>
+      //     ({ ...prevState, [key]: category[key] })));
+      setInitialValues((prevState) => ({
+        ...prevState,
+        ...objKeys,
+      }));
     }
+  }, [id]);
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+  });
 
-    return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} enableReinitialize={true}>
-            {({ errors, touched, isSubmitting, setFieldValue }) => {
-                // useEffect(() => {
-                //     if (id !== 0) {
-                //         console.log('here ');
-                //         const category = categoryItems.filter(category => category._id === id);
-                //         console.log('category ', category);
-                //         const fields = ['name'];
-                //         setFieldValue('name', category['name'], false);
-                //         // fields.forEach(field => setFieldValue(field, category[field], false));
-                //     }
-                // }, [id, setFieldValue]);
+  const onSubmit = async (categoryFields, { resetForm, setSubmitting }) => {
+    console.log("onSubmit ", categoryFields);
+    categoryFields.path =
+      (categoryFields.parent === "" ? "/" : categoryFields.parent) +
+      "/" +
+      categoryFields.name;
+    try {
+      let message = "";
+      if (id === 0) {
+        await dispatch(createCategory(categoryFields)).unwrap();
+        message = "Category Created Successfully!";
+      } else {
+        await dispatch(updateCategory({ id, categoryFields })).unwrap();
+        message = "Category Updated Successfully!";
+      }
+      setStatusMessage((prevState) => ({
+        ...prevState,
+        status: "success",
+        message: message,
+      }));
+      resetForm();
+    } catch (err) {
+      console.error("Failed to save the post: ", err);
+      setStatusMessage((prevState) => ({
+        ...prevState,
+        status: "error",
+        message: err.name + ": " + err.message,
+      }));
+      setSubmitting(false);
+    } finally {
+      setTimeout(() => {
+        setStatusMessage({
+          status: "",
+          message: "",
+        });
+      }, 2000);
+    }
+  };
 
-                return (
-                    <Form>
-                        <div className="modal-body">
-                            <div className="form">
-                                <div className="form-group">
-                                    <label className="mb-1">Category Name :</label>
-                                    <Field name="name" type="text" className={'form-control' + (errors.name && touched.name ? ' is-invalid' : '')} />
-                                    <ErrorMessage name="name" component="div" className="invalid-feedback" />
-                                </div>
-                                <div className="form-group mb-0">
-                                    <label className="mb-1">Parent Category :</label>
-                                    <Field name="parent" as="select" className={'form-control' + (errors.parent && touched.parent ? ' is-invalid' : '')}>
-                                        <option value="/">--Select--</option>
-                                        {
-                                            categoryItems.map(category => (
-                                                <option value={category.path} key={category._id}>{category.name}</option>
-                                            ))
-                                        }
-                                    </Field>
-                                    <ErrorMessage name="parent" component="div" className="invalid-feedback" />
-                                </div>
-                                <div className="form-group status-message">
-                                    {statusMessage?.status === 'success' ? (
-                                        <h5 className="text-success f-w-600">{statusMessage?.message}</h5>
-                                    ) : (
-                                        <h5 className="text-danger f-w-600">{statusMessage?.message}</h5>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="submit" disabled={isSubmitting} className="btn btn-primary">
-                                {isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span> }
-                                {statusMessage?.status === 'success' && <i className="fa fa-check" aria-hidden="true" /> }
-                                Save
-                            </button>
-                            <button className="btn btn-secondary" type="button" onClick={toggleModal}>Close</button>
-                        </div>
-                    </Form>
-                );
-            }}
-        </Formik>
-    )
-}
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      enableReinitialize={true}
+    >
+      {({ errors, touched, isSubmitting, setFieldValue }) => {
+        return (
+          <Form>
+            <div className="modal-body">
+              <div className="form">
+                <div className="form-group">
+                  <label className="mb-1">Category Name :</label>
+                  <Field
+                    name="name"
+                    type="text"
+                    className={
+                      "form-control" +
+                      (errors.name && touched.name ? " is-invalid" : "")
+                    }
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group mb-0">
+                  <label className="mb-1">Parent Category :</label>
+                  <Field
+                    name="parent"
+                    as="select"
+                    className={
+                      "form-control" +
+                      (errors.parent && touched.parent ? " is-invalid" : "")
+                    }
+                  >
+                    <option value="/">--Select--</option>
+                    {categoryItems.map((category) => (
+                      <option value={category.path} key={category._id}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </Field>
+                  <ErrorMessage
+                    name="parent"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group status-message">
+                  {statusMessage?.status === "success" ? (
+                    <h5 className="text-success f-w-600">
+                      {statusMessage?.message}
+                    </h5>
+                  ) : (
+                    <h5 className="text-danger f-w-600">
+                      {statusMessage?.message}
+                    </h5>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="btn btn-primary"
+              >
+                {isSubmitting && (
+                  <span className="spinner-border spinner-border-sm mr-1"></span>
+                )}
+                {statusMessage?.status === "success" && (
+                  <i className="fa fa-check" aria-hidden="true" />
+                )}
+                Save
+              </button>
+              <button
+                className="btn btn-secondary"
+                type="button"
+                onClick={toggleModal}
+              >
+                Close
+              </button>
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 export default CreateEditForm;
--- admin/src/utility/helpers.js
+++ admin/src/utility/helpers.js
@@ -1,35 +1,20 @@
-export function formatFormData (productFields, productImages, existingImages) {
-    const formData = new FormData();
+export function formatFormData(productFields, productImages, existingImages) {
+  const formData = new FormData();
+  const existingImageArr = [];
 
-    Object.keys(productFields).map(fieldName => {
-        formData.append(fieldName, productFields[fieldName]);
-    })
-    console.log('productFields ', productFields);
-    // const formDataObj = {
-    //     info: {
-    //         name: productFields.name,
-    //         sku: productFields.sku,
-    //         category: productFields.category
-    //     },
-    //     detail: {
-    //         model: productFields.model,
-    //         brand: productFields.brand,
-    //         description: productFields.description
-    //     },
-    //     pricing: {
-    //         basePrice: productFields.basePrice
-    //     },
-    //     totalStockCount: productFields.totalStockCount
-    // }
-    // formData.append('data', formDataObj);
-    productImages.length > 0 && productImages.map((image, index) => {
-        formData.append('images', image);
-    });
+  Object.keys(productFields).map((fieldName) => {
+    formData.append(fieldName, productFields[fieldName]);
+  });
 
-    let imageArr = [];
-    existingImages.length > 0 && existingImages.map((image, index) => {
-        imageArr.push(image.preview);
+  productImages.length > 0 &&
+    productImages.map((image) => {
+      formData.append("images", image);
     });
-    formData.append('existingImages', imageArr);
-    return formData;
+  existingImages.length > 0 &&
+    existingImages.map((image) => {
+      existingImageArr.push(image.preview);
+    });
+  formData.append("existingImages", existingImageArr);
+
+  return formData;
 }
