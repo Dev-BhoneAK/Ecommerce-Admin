b230bd2cdbfc481416f3abda2d7862061624e71d
BhoneAK <bhoneak@outlook.com>
2022-07-15 23:36:46 +0630

    feat: Admin UI Implementation - add reviewsData for database seeder

---
+++ backend-api/api-v1/database/data/reviewsData.js
@@ -0,0 +1,26 @@
+exports.reviewsData = [
+  {
+    rating: 2,
+    comment: "This product is not good",
+  },
+  {
+    rating: 4,
+    comment: "This product is pretty good",
+  },
+  {
+    rating: 5,
+    comment: "This product is really good",
+  },
+  {
+    rating: 3,
+    comment: "This product is normal",
+  },
+  {
+    rating: 1,
+    comment: "The worst product",
+  },
+  {
+    rating: 5,
+    comment: "The best product",
+  },
+];
--- backend-api/api-v1/database/seeder.js
+++ backend-api/api-v1/database/seeder.js
@@ -1,62 +1,80 @@
-const mongoose = require('mongoose');
-const {categoriesData} = require('./data/categoriesData');
-const {productsData} = require('./data/productsData');
-const {usersData} = require('./data/usersData');
-const {brandsData} = require('./data/brandsData');
-const User = require('../models/userModel');
-const Category = require('../models/categoryModel');
-const Product = require('../models/productModel');
-const Brand = require('../models/brandModel');
-const database = require('../config/database');
+const mongoose = require("mongoose");
+const { brandsData } = require("./data/brandsData");
+const { categoriesData } = require("./data/categoriesData");
+const { productsData } = require("./data/productsData");
+const { reviewsData } = require("./data/reviewsData");
+const { usersData } = require("./data/usersData");
+const User = require("../models/userModel");
+const Brand = require("../models/brandModel");
+const Category = require("../models/categoryModel");
+const Product = require("../models/productModel");
+const Review = require("../models/reviewModel");
+const database = require("../config/database");
 
-require('dotenv').config();
+require("dotenv").config();
 
 database.connect();
 
 const importData = async () => {
-    try {
-        await Category.deleteMany();
-        await Product.deleteMany();
-        await User.deleteMany();
-        await Brand.deleteMany();
+  try {
+    await Brand.deleteMany();
+    await Category.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
 
-        const createdBrands = await Brand.insertMany(brandsData);
-        const createdUsers = await User.insertMany(usersData);
-        await Category.insertMany(categoriesData);
+    const createdBrands = await Brand.insertMany(brandsData);
+    const createdUsers = await User.insertMany(usersData);
+    await Category.insertMany(categoriesData);
 
-        const adminUser = createdUsers[0]._id;
+    const adminUser = createdUsers[0]._id;
+    const consumerUser = createdUsers[1]._id;
 
-        const products = productsData.map((product, index) => {
-            return { ...product, user: adminUser,  detail: {...product.detail, brand: createdBrands[index]._id}}
-        })
+    const products = productsData.map((product, index) => {
+      return {
+        ...product,
+        user: adminUser,
+        detail: { ...product.detail, brand: createdBrands[index]._id },
+      };
+    });
 
-        await Product.insertMany(products);
+    const createdProducts = await Product.insertMany(products);
 
-        console.log('Data Imported!')
-        process.exit(0);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+    const reviews = reviewsData.map((review, index) => {
+      return {
+        ...review,
+        product: createdProducts[index]._id,
+        user: consumerUser,
+      };
+    });
+
+    await Review.insertMany(reviews);
+
+    console.log("Data Imported Successfully!");
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 const destroyData = async () => {
-    try {
-        await Category.deleteMany();
-        await Product.deleteMany();
-        await User.deleteMany();
-        await Brand.deleteMany();
+  try {
+    await Category.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+    await Brand.deleteMany();
 
-        console.log('Data Destroyed!')
-        process.exit()
-    } catch (error) {
-        console.error(`${error}`)
-        process.exit(1)
-    }
-}
+    console.log("Data Destroyed!");
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
 
-if (process.argv[2] === '-d') {
-    destroyData()
+if (process.argv[2] === "-d") {
+  destroyData();
 } else {
-    importData()
+  importData();
 }
