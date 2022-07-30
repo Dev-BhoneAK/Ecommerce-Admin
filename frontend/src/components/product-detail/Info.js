eda0ff7d21bb1a0e10a10ed2b0c3c0a979d73486
BhoneAK <bhoneak@outlook.com>
2022-07-30 23:49:23 +0630

    feat: Consumer UI Implementation - add loading skeleton component

--- frontend/src/components/product-detail/Info.js
+++ frontend/src/components/product-detail/Info.js
@@ -17,15 +17,6 @@ const Info = () => {
         centerMode: false,
         focusOnSelect: true,
         responsive: [
-            // {
-            //     breakpoint: 1024,
-            //     settings: {
-            //         slidesToShow: 3,
-            //         slidesToScroll: 3,
-            //         infinite: true,
-            //         dots: true
-            //     }
-            // },
             {
                 breakpoint: 600,
                 settings: {
