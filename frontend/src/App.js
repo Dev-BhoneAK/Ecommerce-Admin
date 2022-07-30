ff1a87b46d6b99b1de25b11c1461af92ea06f5d1
BhoneAK <bhoneak@outlook.com>
2022-07-30 23:49:41 +0630

    feat: Consumer UI Implementation - add loading skeleton component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import {Route,Routes} from 'react-router-dom';
 import Home from "./screens/Home";
 import ProductDetail from "./screens/ProductDetail";
+import LoadingSkeleton from "./components/product-detail/LoadingSkeleton";
 
 function App() {
   return  (
       <Routes>
             <Route path="/" exact index element={<Home />} />
-            <Route path="product/:id" element={<ProductDetail />} />
+          {/*<Route path="product/:id" element={<ProductDetail />} />*/}
+          <Route path="/product/:id" element={<LoadingSkeleton />} />
       </Routes>
   );
 }
