07c09672a84f3cf3ba9d2a901585231a7eb87139
BhoneAK <bhoneak@outlook.com>
2022-08-11 23:38:28 +0630

    fix: Consumer UI Implementation - remove header and footer component from ProductDetail component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import {Route,Routes} from 'react-router-dom';
 import Home from "./screens/Home";
 import ProductDetail from "./screens/ProductDetail";
+import Header from "./components/common/Header/Header";
+import Footer from "./components/common/Footer/Footer";
 import LoadingSkeleton from "./components/product-detail/LoadingSkeleton";
 
 function App() {
   return  (
-      <Routes>
-            <Route path="/" exact index element={<Home />} />
-          {/*<Route path="product/:id" element={<ProductDetail />} />*/}
-          <Route path="/product/:id" element={<LoadingSkeleton />} />
-      </Routes>
-  );
+      <>
+          <Header />
+              <Routes>
+                  <Route path="/" exact index element={<Home />} />
+                  <Route path="product/:id" element={<ProductDetail />} />
+                  {/*<Route path="/product/:id" element={<LoadingSkeleton />} />*/}
+              </Routes>
+          <Footer />
+      </>
+
+);
 }
 
 export default App;
--- frontend/src/screens/ProductDetail.js
+++ frontend/src/screens/ProductDetail.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Header from "../components/common/Header/Header";
 import Info from "../components/product-detail/Info";
 import Specification from "../components/product-detail/Specification";
 import ProductListings from "../components/home/ProductListings";
-import Footer from "../components/common/Footer/Footer";
 const ProductDetail = () => {
 
     return (
         <>
-            <Header />
             <Info />
             <Specification />
             <ProductListings title={"Related Products"}/>
-            <Footer />
         </>
     )
 }
