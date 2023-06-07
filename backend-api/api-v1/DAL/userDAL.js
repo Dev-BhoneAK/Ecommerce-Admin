326ec8b4ebd458dfe4d7f5a36c2f5f580ad51494
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-07 23:31:04 +0630

    fix: findUserByCredential

--- backend-api/api-v1/DAL/userDAL.js
+++ backend-api/api-v1/DAL/userDAL.js
@@ -1,30 +1,30 @@
 const User = require("../models/userModel");
 
 exports.findAllUsers = async () => {
-    const users = await User.find();
-    return users;
-}
+  const users = await User.find();
+  return users;
+};
 
 exports.findUserById = async (userId) => {
-    const user = await User.findById(userId);
-    return user;
-}
+  const user = await User.findById(userId);
+  return user;
+};
 
 exports.createUser = (user) => {
-    const newUser = new User(user);
-    return newUser.save();
-}
+  const newUser = new User(user);
+  return newUser.save();
+};
 
 exports.updateUser = async (userId, user) => {
-    return await User.findByIdAndUpdate(userId, user);
-}
+  return await User.findByIdAndUpdate(userId, user);
+};
 
 exports.deleteUser = async (userId) => {
-    return await User.findByIdAndDelete(userId);
-}
+  return await User.findByIdAndDelete(userId);
+};
 
 /* Admin Login */
 exports.findUserByCredentials = async (email, password) => {
-    return await User.findOne({email, password});
-}
+  return await User.findOne({ email });
+};
 /* Admin Login */
