0f7222a2f3a8586d4bf13c812a5b08d45381eb9b
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-08 23:43:31 +0630

    feat:add authSlice

--- admin/src/features/auth/authSlice.js
+++ admin/src/features/auth/authSlice.js
@@ -7,40 +7,10 @@ const initialState = {
     : null,
 };
 
-export const fetchBlogs = createAsyncThunk("blogs/fetchBlogs", async () => {
-  const response = await getAllBlogsAPI();
+export const login = createAsyncThunk("auth/login", async (userCredentials) => {
+  const response = await loginAPI(userCredentials);
   return response.data;
 });
-
-export const getBlogById = (state, blogId) =>
-  state.blogItems.filter((blog) => blog._id === blogId)[0];
-
-export const createBlog = createAsyncThunk(
-  "auth/login",
-  async (userCredentials) => {
-    const response = await loginAPI(userCredentials);
-    return response.data;
-  }
-);
-
-export const updateBlog = createAsyncThunk("blogs/updateBlog", async (blog) => {
-  try {
-    const { id, formData } = blog;
-    const response = await updateBlogAPI(id, formData);
-    return response.data;
-  } catch (e) {
-    console.log("error is ", e);
-  }
-});
-
-export const deleteBlog = createAsyncThunk(
-  "blogs/deleteBlog",
-  async (blogId) => {
-    console.log("arrive delete blog");
-    const response = await deleteBlogAPI(blogId);
-    return response.data;
-  }
-);
 
 const authSlice = createSlice({
   name: "auth",
