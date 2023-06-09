e903e45d0faaeed06ee5301ff7bbc8eee4aa88ba
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-09 23:48:06 +0630

    feat:add comparepassword

--- backend-api/api-v1/models/userModel.js
+++ backend-api/api-v1/models/userModel.js
@@ -1,31 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     roles: {
-        type: Array,
-        required: true
+      type: Array,
+      required: true,
     },
     shippingAddress: {
-        address: {type: String},
-        city: {type: String},
-        state: {type: String},
-        country: {type: String},
-    }
-}, {
-    timestamps: true
-});
+      address: { type: String },
+      city: { type: String },
+      state: { type: String },
+      country: { type: String },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema);
+// userSchema.methods.comparePassword = async (password) => {
+//   try {
+//     console.log("password", password);
+//     return await bcrypt.compare(password, this.password);
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
+
+// // Match user entered password to hashed password in database
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
