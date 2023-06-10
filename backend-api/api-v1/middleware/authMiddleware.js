4e450c9777ff12c31e9aea6f55658f54d2cd5811
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-10 23:43:01 +0630

    feat: add auth middleware

---
+++ backend-api/api-v1/middleware/authMiddleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+// For consumer endpoints
+exports.consumerAuth = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // Read JWT from the 'jwt' cookie
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+// For admin endpoints
+exports.adminAuth = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
