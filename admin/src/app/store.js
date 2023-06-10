f4321e37b6e039396dd187fe0ac599101b29be38
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-10 23:36:01 +0630

    feat: configure auth slice

--- admin/src/app/store.js
+++ admin/src/app/store.js
@@ -1,18 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import blogReducer from "../features/blog/blogSlice";
 import brandReducer from "../features/brand/brandSlice";
 import categoryReducer from "../features/category/categorySlice";
+import orderReducer from "../features/order/orderSlice";
 import productReducer from "../features/product/productSlice";
 import reviewReducer from "../features/product/reviewSlice";
-import orderReducer from "../features/order/orderSlice";
-import blogReducer from "../features/blog/blogSlice";
 
 export default configureStore({
   reducer: {
+    auth: authReducer,
+    blog: blogReducer,
     brand: brandReducer,
     category: categoryReducer,
+    order: orderReducer,
     product: productReducer,
     review: reviewReducer,
-    blog: blogReducer,
-    order: orderReducer,
   },
 });
