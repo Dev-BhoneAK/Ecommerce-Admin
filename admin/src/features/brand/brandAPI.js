67131a2f4fa2a21d087967e7a4305c7f82d39fc0
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-12 23:02:36 +0630

    feat: add jwtInterceptor

--- admin/src/features/brand/brandAPI.js
+++ admin/src/features/brand/brandAPI.js
@@ -1,15 +1,16 @@
-import { API_URL } from "../../config/API";
 import axios from "axios";
 
+import { API_URL } from "../../config/API";
+import jwtInterceoptor from "../../utility/jwtInterceptor";
 const API = API_URL + "brands";
 
 export const getAllBrandsAPI = async () => {
-  return await axios.get(API);
+  return await jwtInterceoptor.get(API);
 };
 
 export const saveBrandAPI = async (brand) => {
   try {
-    const responseData = await axios.post(API, brand, {
+    const responseData = await jwtInterceoptor.post(API, brand, {
       headers: {
         "content-type": "multipart/form-data",
       },
@@ -21,11 +22,11 @@ export const saveBrandAPI = async (brand) => {
 };
 
 export const updateBrandAPI = async (brandId, brand) => {
-  const responseData = await axios.patch(API + `/${brandId}`, brand);
+  const responseData = await jwtInterceoptor.patch(API + `/${brandId}`, brand);
   return responseData;
 };
 
 export const deleteBrandAPI = async (brandId) => {
-  const responseData = await axios.delete(API + `/${brandId}`);
+  const responseData = await jwtInterceoptor.delete(API + `/${brandId}`);
   return responseData;
 };
