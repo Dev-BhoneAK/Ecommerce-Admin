d239cb9fe421217a12c1cfaeab7220dce974294f
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-15 23:49:21 +0630

    feat: add conditional route

--- admin/src/index.js
+++ admin/src/index.js
@@ -4,10 +4,12 @@ import "./css/style.css";
 import "./css/custom-style.css";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import { injectStore } from "./utility/jwtInterceptor";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+injectStore(store); // To use redux store outside react components
 root.render(
   <Provider store={store}>
     <BrowserRouter>
