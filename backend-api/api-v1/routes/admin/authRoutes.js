78bc6bde95df51a50d3aafd15f8202cf29998357
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-16 23:53:00 +0630

    feat: add logout route

--- backend-api/api-v1/routes/admin/authRoutes.js
+++ backend-api/api-v1/routes/admin/authRoutes.js
@@ -7,4 +7,6 @@ router.post("/login", authController.login);
 
 router.post("/refreshToken", authController.refreshToken);
 
+router.post("/logout", authController.logout);
+
 module.exports = router;
