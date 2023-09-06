81693423469ef3fb9d75a53962f038ca6e9cf449
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-06 22:11:59 +0630

    feat: add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import {Route,Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import ProductDetail from "./screens/ProductDetail";
-import Header from "./components/common/Header/Header";
+import Header from "components/common/Header";
 import Footer from "./components/common/Footer/Footer";
 import LoadingSkeleton from "./components/product-detail/LoadingSkeleton";
 
 function App() {
-  return  (
-      <>
-          <Header />
-              <Routes>
-                  <Route path="/" exact index element={<Home />} />
-                  <Route path="product/:id" element={<ProductDetail />} />
-                  {/*<Route path="/product/:id" element={<LoadingSkeleton />} />*/}
-              </Routes>
-          <Footer />
-      </>
-
-);
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" exact index element={<Home />} />
+        <Route path="product/:id" element={<ProductDetail />} />
+        {/*<Route path="/product/:id" element={<LoadingSkeleton />} />*/}
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
