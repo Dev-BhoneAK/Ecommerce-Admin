adbf736fc49b259f4d977d79ba5ee03835bc6b14
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-07 21:52:36 +0630

    feat:add toggle nav

--- frontend/src/components/common/Header/NavBar.js
+++ frontend/src/components/common/Header/NavBar.js
@@ -1,35 +1,51 @@
-import React from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ToggleNavContext } from "context/toggleNavContext";
 
 const NavBar = () => {
-  const handleColor = ({ isActive }) => {
-    return { color: isActive && "red" };
-  };
+  const { isNavOpen, setIsNavOpen } = useContext(ToggleNavContext);
+
   return (
     <div className="col-xxl-6 col-xl-9">
       <div className="main-nav-center">
         <nav className="text-start">
-          <ul className="sm pixelstrap">
+          <ul
+            className="sm pixelstrap sm-horizontal"
+            style={{ right: isNavOpen && "0" }}
+          >
             <li>
-              <div className="mobile-back text-end">
+              <div
+                className="mobile-back text-end"
+                onClick={() => setIsNavOpen(false)}
+              >
                 Back
                 <i className="fa fa-angle-right ps-2" aria-hidden="true" />
               </div>
             </li>
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="/" onClick={() => setIsNavOpen(false)}>
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/brands">Brands</NavLink>
+              <NavLink to="/brands" onClick={() => setIsNavOpen(false)}>
+                Brands
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/collection">Collection</NavLink>
+              <NavLink to="/collection" onClick={() => setIsNavOpen(false)}>
+                Collection
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/shop">Shop</NavLink>
+              <NavLink to="/shop" onClick={() => setIsNavOpen(false)}>
+                Shop
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/blogs">Blogs</NavLink>
+              <NavLink to="/blogs" onClick={() => setIsNavOpen(false)}>
+                Blogs
+              </NavLink>
             </li>
           </ul>
         </nav>
