3f6651c3018b2aac5450d98e4bb3206da1475dda
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-08 23:51:33 +0630

    fix: social icon on product detail page

--- frontend/src/screens/ProductDetail.js
+++ frontend/src/screens/ProductDetail.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 import Info from "../components/product-detail/Info";
 import Specification from "../components/product-detail/Specification";
 import ProductListings from "../components/home/ProductListings";
 const ProductDetail = () => {
-
-    return (
-        <>
-            <Info />
-            <Specification />
-            <ProductListings title={"Related Products"}/>
-        </>
-    )
-}
+  return (
+    <>
+      <Info />
+      <Specification />
+      <ProductListings title={"Related Products"} />
+    </>
+  );
+};
 export default ProductDetail;
