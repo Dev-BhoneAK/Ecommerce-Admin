187fc0e0e8b72151ab83ab06d41849794f4f7c7a
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-13 21:47:26 +0630

    feat: add more data

--- backend-api/api-v1/database/data/brandsData.js
+++ backend-api/api-v1/database/data/brandsData.js
@@ -1,26 +1,66 @@
 exports.brandsData = [
   {
-    name: "Premium",
-    logo: "public/images/brands/premium.png",
-  },
-  {
-    name: "Kate Spade",
-    logo: "public/images/brands/katespade.png",
-  },
-  {
     name: "Apple",
-    logo: "public/images/brands/apple.png",
-  },
-  {
-    name: "ViewSonic",
-    logo: "public/images/brands/viewsonic.png",
+    logo: "/images/brands/apple.png",
   },
   {
     name: "Aukey",
-    logo: "public/images/brands/aukey.png",
+    logo: "/images/brands/aukey.png",
+  },
+  {
+    name: "Dr. Dre Beats",
+    logo: "/images/brands/beats.png",
+  },
+  {
+    name: "Belkin",
+    logo: "/images/brands/belkin.png",
+  },
+  {
+    name: "Casestudi",
+    logo: "/images/brands/casestudi.png",
+  },
+  {
+    name: "Coach",
+    logo: "/images/brands/coach.png",
   },
   {
     name: "Elegant",
-    logo: "public/images/brands/elegant.png",
+    logo: "/images/brands/elegant.png",
+  },
+  {
+    name: "Herman",
+    logo: "/images/brands/herman.png",
+  },
+  {
+    name: "JBL",
+    logo: "/images/brands/jbl.png",
+  },
+  {
+    name: "Kate Spade",
+    logo: "/images/brands/katespade.png",
+  },
+  {
+    name: "Logitech",
+    logo: "/images/brands/logitech.png",
+  },
+  {
+    name: "Premium",
+    logo: "/images/brands/premium.png",
+  },
+  {
+    name: "SONY",
+    logo: "/images/brands/sony.png",
+  },
+  {
+    name: "Switcheasy",
+    logo: "/images/brands/switcheasy.png",
+  },
+  {
+    name: "Verbatim",
+    logo: "/images/brands/verbatim.png",
+  },
+  {
+    name: "ViewSonic",
+    logo: "/images/brands/viewsonic.png",
   },
 ];
