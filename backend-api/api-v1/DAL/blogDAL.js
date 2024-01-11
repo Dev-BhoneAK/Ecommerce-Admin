a62babde273b82c0305249233d96104408f1a6a0
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-01-11 23:56:09 +0630

    fix: change publishedDate on sorting

--- backend-api/api-v1/DAL/blogDAL.js
+++ backend-api/api-v1/DAL/blogDAL.js
@@ -1,31 +1,35 @@
-const blogModel = require('../models/blogModel');
+const { default: mongoose } = require("mongoose");
+const blogModel = require("../models/blogModel");
 const productModel = require("../models/productModel");
 
 exports.findAllBlogs = async () => {
-    const blogs = await blogModel.find().sort({ createdAt: "desc"});
-    return blogs;
-}
+  const blogs = await blogModel.find().sort({ publishedDate: -1 });
+  return blogs;
+};
 
-exports.findBlogById = async (blogId) => {
-    const blog = await blogModel.findById(blogId);
-    return blog;
-}
+exports.qfindBlogById = async (blogId) => {
+  if (!mongoose.Types.ObjectId.isValid(blogId)) {
+    return null;
+  }
+  const blog = await blogModel.findById(blogId);
+  return blog;
+};
 
 exports.createBlog = (blog) => {
-    const newBlog = new blogModel(blog);
-    return newBlog.save();
-}
+  const newBlog = new blogModel(blog);
+  return newBlog.save();
+};
 
 exports.updateBlog = async (blogId, blog) => {
-    return  await blogModel.findByIdAndUpdate(blogId, blog);
-}
+  return await blogModel.findByIdAndUpdate(blogId, blog);
+};
 
 exports.deleteBlog = async (blogId) => {
-    return await blogModel.findByIdAndDelete(blogId);
-}
+  return await blogModel.findByIdAndDelete(blogId);
+};
 
 /* Consumer Endpoint Start */
 exports.getLatestBlogs = async () => {
-    return await blogModel.find().sort({ createdAt: "desc"}).limit(3);
+  return await blogModel.find().sort({ createdAt: "desc" }).limit(3);
 };
 /* Consumer Endpoint End */
