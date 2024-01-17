5fd4b4b36a0ab4a6e321ba2902ca2bab70043fba
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-01-17 21:34:41 +0630

    fix: error handler middleware

--- backend-api/api-v1/middleware/errorHandlerMiddleware.js
+++ backend-api/api-v1/middleware/errorHandlerMiddleware.js
@@ -1,17 +1,24 @@
 exports.notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-}
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
 
 exports.errorHandler = (err, req, res, next) => {
-
-    const statusCode = err.message !== '' ? (res.statusCode === 404 ? res.statusCode : 400) : (res.statusCode || 500);
+  try {
+    const statusCode =
+      err.message !== ""
+        ? res.statusCode === 404
+          ? res.statusCode
+          : 400
+        : res.statusCode || 500;
 
     res.status(statusCode);
     res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-}
-
+      message: err.message,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
