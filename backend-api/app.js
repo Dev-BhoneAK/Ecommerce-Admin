b87f54b08743568c94afe54eb3d23f960b2ff656
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-01-17 21:35:04 +0630

    fix: app.js

--- backend-api/app.js
+++ backend-api/app.js
@@ -11,7 +11,7 @@ const {
 } = require("./api-v1/middleware/errorHandlerMiddleware");
 const database = require("./api-v1/config/database");
 const adminRouter = require("./api-v1/routes/admin");
-
+var usersRouter = require("./api-v1/routes/users");
 // connect to database
 database.connect();
 
@@ -25,8 +25,24 @@ app.use(cookieParser());
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/admin", adminRouter);
+app.use("/users", usersRouter);
 
-app.use(notFound);
-app.use(errorHandler);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  console.log("Err ", err);
+  res.status(err.status || 500);
+  res.json({
+    status: "failure",
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+// app.use(notFound);
+// app.use(errorHandler);
 
 module.exports = app;
