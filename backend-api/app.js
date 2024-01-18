e11f3379c3c432de7ee3f749a8c5b30726e590ee
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-01-18 21:31:32 +0630

    fix: remove default error handler

--- backend-api/app.js
+++ backend-api/app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:8888", credentials: true }));
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json());  
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -28,21 +28,7 @@ app.use("/api/v1/admin", adminRouter);
 app.use("/users", usersRouter);
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  console.log("Err ", err);
-  res.status(err.status || 500);
-  res.json({
-    status: "failure",
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-});
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
